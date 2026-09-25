b73031e44146a948289129fa33018a2b7d6729c4
khaoula elouarity <khaoulaelouarity@gmail.com>
2026-09-25 22:45:25 +0200

    fix: call connectDB inside server.js for vercel

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
 
+// متغير لحفظ حالة الاتصال فـ الذاكرة (Caching) باش ما يبقاش يحل اتصال جديد فكل مرة
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      family: 4,
-      serverSelectionTimeoutMS: 10000,
-      autoIndex: process.env.NODE_ENV !== 'production',
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}/${conn.connection.name}`);
-  } catch (error) {
-    console.error(`MongoDB Connection Error: ${error.message}`);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false, // 👈 هادي هي الأداة السحرية اللي كتحبس داك الـ Timeout
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 };
 
 module.exports = connectDB;

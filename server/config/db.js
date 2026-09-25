4ef424f72eb7eab02addb1c8904650527c8fa535
khaoula elouarity <khaoulaelouarity@gmail.com>
2026-09-25 11:30:34 +0200

    Fix MongoDB connection string and IPv4 resolution

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
+      family: 4,
       serverSelectionTimeoutMS: 10000,
       autoIndex: process.env.NODE_ENV !== 'production',
     });
